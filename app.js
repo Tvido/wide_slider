2d5c929df455e8a1d8154d4985f03ba21dcd331a
Volodymyr Viter <mad.tvido@gmail.com>
2021-09-26 23:32:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeIndx = 2) {
+  const slides = document.querySelectorAll('.slide');
 
-for (const slide of slides) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses();
-    slide.classList.add('active');
-  });
+  slides[activeIndx].classList.add('active');
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+      slide.classList.add('active');
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach(slide => {
+      slide.classList.remove('active');
+    });
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach(slide => {
-    slide.classList.remove('active');
-  });
-}
+slidesPlugin();
